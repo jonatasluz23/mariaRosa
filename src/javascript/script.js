e4538146d10373ed4f4d79dfa15dcbd2f996fc91
Jônatas <jonatasmluz@gmail.com>
2024-10-07 17:48:17 -0300

    Add files via upload

---
+++ src/javascript/script.js
@@ -0,0 +1,14 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach(item => {
+    item.addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+});
